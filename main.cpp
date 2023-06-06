8eb0d79b8e507b0c0c074510778189121172aa61
Clovicorn <109252472+Clovicorn@users.noreply.github.com>
2023-06-06 14:25:27 -0700

    Updated to move in 8 dirs. Added space to pause.

--- main.cpp
+++ main.cpp
@@ -9,13 +9,16 @@ int main(void)
 {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window *mWindow = SDL_CreateWindow("Random Walk", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 360, SDL_WINDOW_SHOWN);
+    SDL_Window *mWindow = SDL_CreateWindow("Random Walk", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 320 * 3, 180 * 3, SDL_WINDOW_SHOWN);
 
     SDL_Surface *mSurface = SDL_GetWindowSurface(mWindow);
+    SDL_Surface *mBackSurface = SDL_CreateRGBSurface(0, 320, 180, 32, 0, 0, 0, 0);
     SDL_FillRect(mSurface, nullptr, SDL_MapRGB(mSurface->format, 0, 0, 0));
+    SDL_FillRect(mBackSurface, nullptr, SDL_MapRGB(mSurface->format, 0, 0, 0));
 
     SDL_Event event;
     bool isRunning = true;
+    bool isPaused = false;
     int x = 640 / 4;
     int y = 360 / 4;
     while (isRunning)
@@ -26,28 +29,30 @@ int main(void)
             {
                 isRunning = false;
             }
+            if (event.type == SDL_KEYDOWN)
+            {
+                if (event.key.keysym.sym == SDLK_SPACE)
+                {
+                    isPaused = !isPaused;
+                }
+            }
         }
-        int randNum = (std::rand() % 4);
-        if (randNum == 0)
+        if (!isPaused)
         {
-            ++x;
+            std::srand(static_cast<unsigned int>(SDL_GetTicks() * (std::rand() % (RAND_MAX / 2)) + 1));
+            int randY = (std::rand() % 3) - 1;
+            y += randY;
+            SDL_Delay(std::rand() % 100);
+            std::srand(static_cast<unsigned int>((SDL_GetTicks64() % (std::rand() % RAND_MAX) * std::rand())));
+            int randX = (std::rand() % 3) - 1;
+            x += randX;
+            DrawOnSurface(*mBackSurface, x, y);
+            SDL_BlitScaled(mBackSurface, nullptr, mSurface, nullptr);
+            SDL_UpdateWindowSurface(mWindow);
         }
-        if (randNum == 1)
-        {
-            --x;
-        }
-        if (randNum == 2)
-        {
-            ++y;
-        }
-        if (randNum == 3)
-        {
-            --y;
-        }
-        DrawOnSurface(*mSurface, x, y);
-        SDL_UpdateWindowSurface(mWindow);
     }
 
+    SDL_FreeSurface(mBackSurface);
     SDL_FreeSurface(mSurface);
     SDL_DestroyWindow(mWindow);
     return 0;
@@ -57,14 +62,34 @@ void DrawOnSurface(SDL_Surface &surface, int x, int y)
 {
     int width = surface.w;
     int height = surface.h;
-    if (x >= width || x <= 0 || y <= 0 || y >= height)
+    if (x > width)
     {
-        return;
+        x = width;
     }
+    if (x < 0)
+    {
+        x = 0;
+    }
+    if (y < 0)
+    {
+        y = 1;
+    }
+    if (y > height)
+    {
+        y = height - 1;
+    }
+
     int index = y * width + x;
-    uint32_t white = SDL_MapRGB(surface.format, 255, 255, 255);
+    uint32_t red = SDL_MapRGB(surface.format, 175, 0, 0);
     SDL_LockSurface(&surface);
     uint32_t *pixels = (uint32_t *)surface.pixels;
-    pixels[index] = white;
+    if (pixels[index] != red)
+    {
+        pixels[index] = red;
+    }
+    else
+    {
+        pixels[index] = SDL_MapRGB(surface.format, 175, 0x8B, 0);
+    }
     SDL_UnlockSurface(&surface);
 }
